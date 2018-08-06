8b66ebff0b989e83f6c193415b561f9a107a0d37
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-08-06 20:43:13 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,7 @@
-def square_array(my_array)
-  my_array.collect{|num| num**2 }
- 
-   
+def square_array(numbers)
+  # your code here	  new_numbers = []
+end 	  numbers.each do |num|
+    new_numbers << (num ** 2)
+  end
+  return new_numbers
 end
-
